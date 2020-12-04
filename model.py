2cbd0d0091f1b839a75b39228064fd91ddca98a4
sang <sanghuynh1501>
2020-12-04 14:21:10 +0700

    first commit

---
+++ model.py
@@ -0,0 +1,211 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Dropout, LayerNormalization
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+
+devices = tf.config.experimental.list_physical_devices("GPU")
+
+try:
+    tf.config.experimental.set_memory_growth(devices[0], True)
+except:
+    print("no GPU found")
+
+
+def gelu(x):
+    return 0.5*x*(1+tf.tanh(np.sqrt(2/np.pi)*(x+0.044715*tf.pow(x, 3))))
+
+
+def scaled_dot_product_attention(q, k, v, mask):
+    """Calculate the attention weights.
+      q, k, v must have matching leading dimensions.
+      k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
+      The mask has different shapes depending on its type(padding or look ahead)
+      but it must be broadcastable for addition.
+
+      Args:
+        q: query shape == (..., seq_len_q, depth)
+        k: key shape == (..., seq_len_k, depth)
+        v: value shape == (..., seq_len_v, depth_v)
+        mask: Float tensor with shape broadcastable
+              to (..., seq_len_q, seq_len_k). Defaults to None.
+
+      Returns:
+        output, attention_weights
+      """
+
+    matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
+
+    # scale matmul_qk
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    # add the mask to the scaled tensor.
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    # softmax is normalized on the last axis (seq_len_k) so that the scores
+    # add up to 1.
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)  # (..., seq_len_q, seq_len_k)
+
+    output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
+
+    return output, attention_weights
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.wq = tf.keras.layers.Dense(d_model)
+        self.wk = tf.keras.layers.Dense(d_model)
+        self.wv = tf.keras.layers.Dense(d_model)
+
+        self.dense = tf.keras.layers.Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        """Split the last dimension into (num_heads, depth).
+    Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
+    """
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, v, k, q, mask):
+        batch_size = tf.shape(q)[0]
+
+        q = self.wq(q)  # (batch_size, seq_len, d_model)
+        k = self.wk(k)  # (batch_size, seq_len, d_model)
+        v = self.wv(v)  # (batch_size, seq_len, d_model)
+
+        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
+        k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len_k, depth)
+        v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len_v, depth)
+
+        # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
+        # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
+        scaled_attention, attention_weights = scaled_dot_product_attention(
+            q, k, v, mask)
+
+        scaled_attention = tf.transpose(scaled_attention,
+                                        perm=[0, 2, 1, 3])  # (batch_size, seq_len_q, num_heads, depth)
+
+        concat_attention = tf.reshape(scaled_attention,
+                                      (batch_size, -1, self.d_model))  # (batch_size, seq_len_q, d_model)
+
+        output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
+
+        return output, attention_weights
+
+
+def point_wise_feed_forward_network(d_model, dff):
+    return tf.keras.Sequential([
+        tf.keras.layers.Dense(dff, activation='relu'),  # (batch_size, seq_len, dff)
+        tf.keras.layers.Dense(d_model)  # (batch_size, seq_len, d_model)
+    ])
+
+
+class EncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(EncoderLayer, self).__init__()
+
+        self.mha = MultiHeadAttention(d_model, num_heads)
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, mask):
+        attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
+
+        ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layernorm2(out1 + ffn_output)  # (batch_size, input_seq_len, d_model)
+
+        return out2
+
+
+class VisionTransformer(tf.keras.Model):
+    def __init__(
+            self,
+            image_size,
+            patch_size,
+            num_layers,
+            num_classes,
+            d_model,
+            num_heads,
+            mlp_dim,
+            channels=3,
+            dropout=0.1,
+    ):
+        super(VisionTransformer, self).__init__()
+        num_patches = (image_size // patch_size) ** 2
+        self.patch_dim = channels * patch_size ** 2
+
+        self.patch_size = patch_size
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.pos_emb = self.add_weight(
+            "pos_emb", shape=(1, num_patches + 1, d_model)
+        )
+        self.class_emb = self.add_weight("class_emb", shape=(1, 1, d_model))
+        self.patch_proj = Dense(d_model)
+        self.enc_layers = [
+            EncoderLayer(d_model, num_heads, mlp_dim, dropout)
+            for _ in range(num_layers)
+        ]
+        self.mlp_head = tf.keras.Sequential(
+            [
+                LayerNormalization(epsilon=1e-6),
+                Dense(mlp_dim, activation=gelu),
+                Dropout(dropout),
+                Dense(num_classes),
+            ]
+        )
+
+    def extract_patches(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patches = tf.reshape(patches, [batch_size, -1, self.patch_dim])
+        return patches
+
+    def call(self, x, training):
+        batch_size = tf.shape(x)[0]
+        patches = self.extract_patches(x)
+        x = self.patch_proj(patches)
+
+        class_emb = tf.broadcast_to(
+            self.class_emb, [batch_size, 1, self.d_model]
+        )
+        x = tf.concat([class_emb, x], axis=1)
+        x = x + self.pos_emb
+
+        for layer in self.enc_layers:
+            x = layer(x, training, None)
+
+        # First (class token) is used for classification
+        x = self.mlp_head(x[:, 0])
+        return x
+
+
+if __name__ == '__main__':
+    image = np.ones((1, 32, 32, 1))
+    model = VisionTransformer(32, 4, 4, 10, 64, 4, 128, 1)
+    labels = model(image, False)
+    print(labels.shape)
